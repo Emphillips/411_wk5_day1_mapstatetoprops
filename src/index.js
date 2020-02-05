6dbafdd09689c9b14006e285ac62fee736e5c4d6
emphillips <matt-phillips@live.com>
2020-02-05 14:45:38 -0600

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+
+
+
+//STORE -> GLOBALIZED STATE - Hold all state
+//We used one piece of stat *counter* (REDUCER)ie Auth reducer, search reducer.
+//
+
+
+//ACTION INCREMENT ie I'm hungry
+//Simple function that returns an object
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+
+//REDUCER
+//Simple functions that returns an object
+const counter = (state = 0, action) => {
+      switch (action.type){
+        case 'INCREMENT':
+          return state + 1;
+        case 'DECREMENT':
+          return state - 1;
+      }
+};
+
+let store = createStore(counter);
+
+//Display in the console
+store.subscribe(() => console.log(store.getState()));
+
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
